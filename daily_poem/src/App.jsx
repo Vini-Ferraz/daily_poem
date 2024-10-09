4778e7ce3fe649f36f5a5f8fdfd8004d2bfd14e0
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2024-10-09 10:02:14 -0300

    Criando componente do poemas
    Crie o componente do poema, mas precisei fazer um lift state para que todos os componentes recebam as informações.

--- daily_poem/src/App.jsx
+++ daily_poem/src/App.jsx
@@ -1,8 +1,23 @@
 import "./App.css";
+import { Poem } from "./components/poemDisplay";
 
 function App() {
+  const poem = [
+    {
+      poem: "Beneath the calm, soft winds play,\nQuiet thoughts in gentle sway.\nTime slows down, a soothing stream—\nHearts in dream.",
+      emotion: "peace",
+      flower: "lily",
+      hasBeenRead: false,
+    },
+    {
+      poem: "In the fire’s glow, passion burns,\nDesires grow as the world turns.\nHearts ignite, a burning flame—\nLove's sweet game.",
+      emotion: "desire",
+      flower: "rose",
+      hasBeenRead: false,
+    },
+  ];
 
-  return <h1>Hello World!</h1>;
+  return <Poem poem={poem[1].poem} />;
 }
 
 export default App;
---
+++ daily_poem/src/components/poemDisplay.jsx
@@ -0,0 +1,5 @@
+//Example of the poem object structure;
+
+export function Poem({ poem }) {
+  return <p>{poem}</p>;
+}
