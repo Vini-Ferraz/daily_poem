7318ef651e1777d0b9809b584a52fcfa7db39a0d
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2024-10-14 16:50:05 -0300

    Criando a base da interação

--- daily_poem/src/components/flowers/rose.jsx
+++ daily_poem/src/components/flowers/rose.jsx
@@ -1,3 +1,67 @@
+import { useState, useRef } from "react";
+
+const rainSound = new Audio("sound-effects/rain.mp3");
+
 export function Rose() {
-  return <img src="rose.webp" alt="Rose" className="rose-image" />;
+  const [isDark, setIsDark] = useState(false);
+  const rainInterval = useRef(null); // UseRef para armazenar o intervalo
+
+  const handleMouseDown = () => {
+    rainSound.play();
+    // Inicia a criação das gotas de chuva repetidamente a cada 100ms
+    rainInterval.current = setInterval(() => {
+      createRainDrop();
+    }, 20);
+
+    // Escurece a tela
+    setIsDark(true);
+  };
+
+  const handleMouseUp = () => {
+    rainSound.pause();
+    // Para de escurecer a tela
+    setIsDark(false);
+
+    // Limpa o intervalo, parando a criação de novas gotas de chuva
+    clearInterval(rainInterval.current);
+  };
+
+  const createRainDrop = () => {
+    const darkOverlay = document.querySelector(".dark-overlay");
+
+    // Verifica se a dark-overlay existe
+    if (!darkOverlay) {
+      console.error("dark-overlay não encontrada");
+      return; // Se não encontrar a div, sai da função
+    }
+
+    const rainDrop = document.createElement("div");
+    rainDrop.className = "rain-drop";
+
+    const randomX = Math.floor(Math.random() * (window.innerWidth - 10));
+    rainDrop.style.left = `${randomX}px`;
+
+    darkOverlay.appendChild(rainDrop);
+
+    setTimeout(() => {
+      rainDrop.remove();
+    }, 2000);
+  };
+
+  return (
+    <>
+      <div className={`dark-overlay ${isDark ? "active" : ""}`}></div>
+
+      <img
+        src="rose.webp"
+        alt="Rose"
+        className="rose-image"
+        style={{ cursor: "pointer" }}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onTouchStart={handleMouseDown}
+        onTouchEnd={handleMouseUp}
+      />
+    </>
+  );
 }
